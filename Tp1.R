2bee679bbc17037c71fc4fef4731d930212e496d
NicolasFBNG <123750873+NicolasFBNG@users.noreply.github.com>
2023-02-01 21:24:04 +0100

    Update Tp1.R

--- Tp1.R
+++ Tp1.R
@@ -1,23 +1,30 @@
 brutToNet1 <- function(brut){
-  if(is.numeric(brut)){ net <- brut - 0.22 * brut
+  if(is.numeric(brut)){ net <- brut - 0.22 * brut  #Calcule directement le slaire mensuel net 
   return(net)}
   else {
-    return("ERROR: type not expected")}
+    return("ERROR: type not expected")}            #Si la valeur envoyée n'est pas numérique le code renvoie: "ERROR: type not expected"
 }
 
-brutToNet2 <- function(brut,cadres){
-  if(is.numeric(brut)&cadres=="non-cadres"){net <- brut*0.78*0.925
+brutToNet2 <- function(brut,cadre){
+  if (!is.character(cadres))                                         #On vérifie si les valeurs entrée sont conforment
+    {return( “ERROR : contract unknown”)
+  if (!is.numeric(brut))
+    {return( “ERROR : type not expected”)}
+  if(cadre=="non-cadre"){net <- brut*0.78*0.925                      #On créer deux conditions pour faire des calcules différents en fonction de si on calcule pour un cadre ou non
   return(net)}
-  if(is.numeric(brut)&cadres=="cadres"){ net <- brut*0.75*0.925
+  if(cadre=="cadre"){ net <- brut*0.75*0.925
   return(net)}
-  else {return("ERROR: type not expected")}
 }
 
-brutToNet3 <- function(brut, cadres, taux = 7.5, temps = 100){
-  if(!is.numeric(brut) || !is.character(cadres) || temps < 0 || temps > 100 || taux< 0 || taux > 100)
+brutToNet3 <- function(brut, cadre, taux = 7.5, temps = 100){
+  if temps < 0 || temps > 100 || taux< 0 || taux > 100)              # On vérifie si toutes les valeurs entrée sont conformes
     {return("ERROR: rate and time must be in range(0,100)")}
-  if(cadres=="non-cadres"){ net_avant_impots <- brut*0.78}
-  if(cadres=="cadres"){ net_avant_impots <- brut*0.75}
-  net_après_impots<-net_avant_impots*(1-taux/100)*(temps/100)
+  if (!is.character(cadres))
+    {return( “ERROR : contract unknown”)
+  if (!is.numeric(brut))
+    {return( “ERROR : type not expected”)}
+  if(cadre=="non-cadre"){ net_avant_impots <- brut*0.78}             #On calcule le salaire net avant les impots en fonctions de "cadres" ou "non-cadres"
+  if(cadre=="cadre"){ net_avant_impots <- brut*0.75}
+  net_après_impots<-net_avant_impots*(1-taux/100)*(temps/100)        #On calcule le salaire net après impots avec le salaires net avant les impots 
   return(list(net_avant_impots,net_après_impots))
 }
